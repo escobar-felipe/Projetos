a051e4eef3d741059c0d85600e84aa07104d7fb6
escobar-felipe <felipe_6512@hotmail.com>
2022-01-18 15:24:51 -0300

    atualizando projeto

--- Suicidio_brasil/app.py
+++ Suicidio_brasil/app.py
@@ -203,11 +203,6 @@ st.set_page_config(
      page_icon="chart_with_upwards_trend",
      layout="centered",
      initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
-     }
  )
 
 
